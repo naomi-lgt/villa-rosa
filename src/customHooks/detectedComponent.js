9b4db95dac52a6573301255df414ed0a7c5a63e0
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-06-20 18:58:20 +0200

    fixed desktop fonts and scaling + before installing react-stacked-carousel

---
+++ src/customHooks/detectedComponent.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+
+const detectedComponent = (ref, callback) => {
+    const handleViewport = () => {
+        if (ref.current && !ref.current.contains(e.target)) {
+            // callback();
+            console.log('in handleViewport');
+          }
+    }
+
+    useEffect(() => {
+        document.addEventListener()
+    })
+};
+
+export default detectedComponent;
