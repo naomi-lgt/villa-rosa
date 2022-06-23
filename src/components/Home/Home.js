0c15641a317afbebc3a3eda5c288cdafd40dc8e4
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-06-24 01:13:53 +0200

    deleted react-stack-carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,4 @@
-import { useRef, StyleSheet } from 'react';
-import { StackedCarousel } from 'react-stacked-carousel'
-import 'react-stacked-carousel/dist/index.css';
-
+import { useRef } from 'react';
 import './Home.scss';
 
 const Home = () => {
@@ -14,37 +11,10 @@ const Home = () => {
                 <p>Since 2003, Villa Rosa welcomes you to their restaurant to offer a one of a kind experience.</p>
             </div>
             <div className="home-carousel-container">
-                <div className="home-carousel">
-                <StackedCarousel
-                    autoRotate={false}
-                    rotationInterval={3000}
-                    containerClassName={"carousel"}
-                    cardClassName="card"
-                    leftButton={<button>Left</button>}
-                    rightButton={<button>Right</button>}
-                    >
-                    <div className="card" key={'child1'}>
-                    <h2>First Card</h2>
-
-                    </div>
-                    <div className="card" key={'child2'}>
-                    <h2>Second Card</h2>
-
-                    </div>
-                    <div className="card" key={'child3'}>
-                    <h2>Third Card</h2>
-
-                    </div>
-                    <div className="card" key={'child4'}>
-                    <h2>Fourth Card</h2>
-
-                    </div>
-                    <div className="card" key={'child5'}>
-                    <h2>Fifth Card</h2>
-
-                    </div>
-
-                </StackedCarousel>
+                <div className="home-carousel-container home-carousel">
+                    <div className="pic1"></div>
+                    <div className="pic2"></div>
+                    <div className="pic3"></div>
                 </div>
             </div>
         </div>
