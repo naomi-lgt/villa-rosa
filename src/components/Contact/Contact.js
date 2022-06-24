8cd668997264279a99c58c8cb552973643c1d03d
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-06-24 02:07:53 +0200

    fixed some styling bugs and used swiper js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,16 +7,18 @@ const Contact = () => {
                 <h1>Contact</h1>
                 <p>Here are all the information you need to contact us.</p>
             </div>
-            <div className="contact-info">
-                <h2>Opening hours</h2>
-                <p><em>Monday - Friday:</em> 11h30 - 14h30 18h30 - 23h</p>
-                <p><em>Saturday:</em> 11h30 - 23h</p>
-                <h2>Telephone</h2>
-                <p>01632 960765</p>
-                <h2>Email</h2>
-                <p>booking@villarosa.uk</p>
-                <h2>Facebook</h2>
-                <p>Villa Rosa</p>
+            <div className="contact-info-container">
+                <div className="contact-info">
+                    <h2>Opening hours</h2>
+                    <p><em>Monday - Friday:</em> 11h30 - 14h30 18h30 - 23h</p>
+                    <p><em>Saturday:</em> 11h30 - 23h</p>
+                    <h2>Telephone</h2>
+                    <p>01632 960765</p>
+                    <h2>Email</h2>
+                    <p>booking@villarosa.uk</p>
+                    <h2>Facebook</h2>
+                    <p>Villa Rosa</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,9 @@
 import { useRef } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-cards";
+
 import './Home.scss';
 
 const Home = () => {
@@ -11,10 +16,28 @@ const Home = () => {
                 <p>Since 2003, Villa Rosa welcomes you to their restaurant to offer a one of a kind experience.</p>
             </div>
             <div className="home-carousel-container">
-                <div className="home-carousel-container home-carousel">
-                    <div className="pic1"></div>
-                    <div className="pic2"></div>
-                    <div className="pic3"></div>
+                <div className="home-carousel">
+                <Swiper
+                    effect={"cards"}
+                    loop={true}
+                    autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                    }}
+                    grabCursor={true}
+                    modules={[EffectCards, Autoplay]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
                 </div>
             </div>
         </div>
